aa0f66c872159c0f5152af39c78cab38e2b346c8
cbeaubrun6 <cbeaubrun6@gmail.com>
2025-10-05 22:09:48 -0400

    Update R09_LCK05_J.java

--- R09_LCK05_J.java
+++ R09_LCK05_J.java
@@ -1,10 +1,13 @@
 public class R09_LCK05_J {
-    /* This class is not thread-safe */
+  /* This class is thread-safe */
     public final class CountHits {
         private static int counter;
-        
+        private static final Object lock = new Object();
+ 
         public void incrementCounter() {
+            synchronized (lock) {
             counter++;
+            }
         }
     }
 }
