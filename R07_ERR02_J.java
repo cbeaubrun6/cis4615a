8759d671b84a57d5fe12d94d8670f25d5d042ced
cbeaubrun6 <cbeaubrun6@gmail.com>
2025-10-05 22:03:09 -0400

    Update R07_ERR02_J.java

--- R07_ERR02_J.java
+++ R07_ERR02_J.java
@@ -1,8 +1,10 @@
+import java.util.logging.Level;
+
 public class R07_ERR02_J {
     try {
     // ...
-    } catch (SecurityException se) {
-    System.err.println(se);
+    } catch(SecurityException se) {
+    logger.log(Level.SEVERE, se);
     // Recover from exception
     }
 }
