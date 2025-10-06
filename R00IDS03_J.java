1ddeaabf80416b35734221f163d3d2ff9259b6f4
cbeaubrun6 <cbeaubrun6@gmail.com>
2025-10-05 21:10:59 -0400

    Create R00IDS03_J.java

---
+++ R00IDS03_J.java
@@ -0,0 +1,7 @@
+public class R00IDS03_J {
+    if (loginSuccessful) {
+        logger.severe("User login succeeded for: " + username);
+    } else {
+        logger.severe("User login failed for: " + username);
+    }
+}
