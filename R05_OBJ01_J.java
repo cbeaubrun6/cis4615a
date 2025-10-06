b6291d7bb8db4215909b0f0e04c8448c41d01920
cbeaubrun6 <cbeaubrun6@gmail.com>
2025-10-05 22:00:09 -0400

    Create R05_OBJ01_J.java

---
+++ R05_OBJ01_J.java
@@ -0,0 +1,23 @@
+public class R05_OBJ01_J {
+    public class Widget {
+        public int total; // Number of elements
+    
+        void add() {
+            if (total < Integer.MAX_VALUE) {     
+            total++;
+            // ...
+            } else {
+            throw new ArithmeticException("Overflow");
+            }
+        }
+    
+        void remove() { 
+            if (total > 0) {     
+            total--;
+            // ...
+            } else {
+            throw new ArithmeticException("Overflow");
+            }
+        }
+    }
+}
