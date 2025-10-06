c3596ba11818bf56f1676c8ea1a76e10c934ca03
cbeaubrun6 <cbeaubrun6@gmail.com>
2025-10-05 22:00:39 -0400

    Update R05_OBJ01_J.java

--- R05_OBJ01_J.java
+++ R05_OBJ01_J.java
@@ -1,23 +1,11 @@
 public class R05_OBJ01_J {
     public class Widget {
-        public int total; // Number of elements
+    private int total; // Declared private
     
-        void add() {
-            if (total < Integer.MAX_VALUE) {     
-            total++;
-            // ...
-            } else {
-            throw new ArithmeticException("Overflow");
-            }
-        }
+    public int getTotal () {
+        return total;
+    }
     
-        void remove() { 
-            if (total > 0) {     
-            total--;
-            // ...
-            } else {
-            throw new ArithmeticException("Overflow");
-            }
-        }
+    // Definitions for add() and remove() remain the same
     }
 }
